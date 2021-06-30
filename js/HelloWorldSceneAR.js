b054c41cf7656519a3e29d1f9da22d5cfdb6da64
gabe <ggetzie@gmail.com>
2021-06-30 12:26:55 +0800

    change message

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -35,7 +35,7 @@ export default class HelloWorldSceneAR extends Component {
   _onInitialized(state, reason) {
     if (state == ViroConstants.TRACKING_NORMAL) {
       this.setState({
-        text : "Hello World!"
+        text : "Alternate History DC"
       });
     } else if (state == ViroConstants.TRACKING_NONE) {
       // Handle loss of tracking
